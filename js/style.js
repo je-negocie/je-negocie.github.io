e480a408139b8b2fff37afe04b9dda2b4ef23910
je-negocie <129177594+je-negocie@users.noreply.github.com>
2023-03-31 14:53:41 +0800

    Add files via upload

---
+++ js/style.js
@@ -0,0 +1,48 @@
+  function display(id){  
+        var traget=document.getElementById(id);  
+        if(traget.style.display=="none"){  
+            traget.style.display="";  
+        }else{  
+            traget.style.display="none";  
+      }  
+   }
+  function display(id1){  
+        var traget=document.getElementById(id1);  
+        if(traget.style.display=="none"){  
+            traget.style.display="";  
+        }else{  
+            traget.style.display="none";  
+      }  
+   }
+ function display(id2){  
+        var traget=document.getElementById(id2);  
+        if(traget.style.display=="none"){  
+            traget.style.display="";  
+        }else{  
+            traget.style.display="none";  
+      }  
+   }
+ function display(id3){  
+        var traget=document.getElementById(id3);  
+        if(traget.style.display=="none"){  
+            traget.style.display="";  
+        }else{  
+            traget.style.display="none";  
+      }  
+   }
+ function display(id4){  
+        var traget=document.getElementById(id4);  
+        if(traget.style.display=="none"){  
+            traget.style.display="";  
+        }else{  
+            traget.style.display="none";  
+      }  
+   }
+ function display(id5){  
+        var traget=document.getElementById(id5);  
+        if(traget.style.display=="none"){  
+            traget.style.display="";  
+        }else{  
+            traget.style.display="none";  
+      }  
+   }
